c4cc47058269a236fb6cde7ab87214cd786148be
EugeniuPeruzzi <jaka1997@live.it>
2023-05-17 18:13:20 +0200

    small adjusments

--- js/script.js
+++ js/script.js
@@ -13,20 +13,21 @@ let under_18_price;
 let over_65_price;
 
 // Definisco i casi
-if (isNaN(travel_distance) && isNaN(user_age)){
+if (isNaN(travel_distance) || isNaN(user_age)){
     alert('Uno dei due numeri non e un numero');
-
-
-}else if (user_age < 18){
-    under_18_price = (full_price - (full_price * 20 / 100)).toFixed(2);
-    document.getElementById('ticket_price').innerHTML = `Il prezzo under 18 e di ${under_18_price}$`;
-
-}else if (user_age > 65){
-    over_65_price = (full_price - (full_price * 40 / 100)).toFixed(2);
-    document.getElementById('ticket_price').innerHTML = `Il prezzo over 65 e di ${over_65_price}$`;
-
-}else {
-    document.getElementById('ticket_price').innerHTML = `Il prezzo Intero e di ${full_price}$`;
 }
 
+else if (user_age < 18){
+    under_18_price = (full_price - (full_price * 20 / 100)).toFixed(2);
+    document.getElementById('ticket_price').innerHTML = `Il prezzo under 18 e di ${under_18_price}$`;
+}
+
+else if (user_age > 65){
+    over_65_price = (full_price - (full_price * 40 / 100)).toFixed(2);
+    document.getElementById('ticket_price').innerHTML = `Il prezzo over 65 e di ${over_65_price}$`;
+}
+
+else {
+    document.getElementById('ticket_price').innerHTML = `Il prezzo Intero e di ${full_price}$`;
+}
 // Stampo a schermo il risultato
